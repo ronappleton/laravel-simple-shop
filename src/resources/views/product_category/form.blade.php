75840931653c9e277e1477afdaa870c4e87e9fb0
Ron Appleton <ron.appleton@gmail.com>
2017-12-13 22:28:13 +0000

    Makes form uni-useful

--- src/resources/views/product_category/form.blade.php
+++ src/resources/views/product_category/form.blade.php
@@ -1,11 +1,11 @@
 <div class="box box-success">
     <div class="box-header with-border">
-        <h3 class="box-title">New Category</h3>
+        <h3 class="box-title">{{ empty($model) ? 'New Category' : 'Alter Category' }}</h3>
     </div>
     <div class="box-body">
         <div class="input-group {{ $errors->has('name') ? 'has-error' : '' }}">
             <span class="input-group-addon"><i class="fa fa-motorcycle" aria-hidden="true"></i></span>
-            {{ Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Name...']) }}
+            {{ Form::text('name', empty($model) ? old('name') : $model->name, ['class' => 'form-control', 'placeholder' => 'Name...']) }}
             @if ($errors->has('name'))
                 <span class="help-block">
                             <strong>{{ $errors->first('name') }}</strong>
