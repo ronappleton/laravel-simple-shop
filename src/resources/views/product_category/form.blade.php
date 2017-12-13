92b7501a61ba622cb40ebf7bf731e2a484edc631
Ron Appleton <ron.appleton@gmail.com>
2017-12-13 22:08:15 +0000

    .

--- src/resources/views/product_category/form.blade.php
+++ src/resources/views/product_category/form.blade.php
@@ -1,10 +1,10 @@
-<div class="box box-info">
+<div class="box box-success">
     <div class="box-header with-border">
-        <h3 class="box-title">Input Addon</h3>
+        <h3 class="box-title">New Category</h3>
     </div>
     <div class="box-body">
         <div class="input-group {{ $errors->has('name') ? 'has-error' : '' }}">
-            <span class="input-group-addon"><i class="material-icons">motorcycle</i></span>
+            <span class="input-group-addon"><i class="fa fa-motorcycle" aria-hidden="true"></i></span>
             {{ Form::text('name', old('name'), ['class' => 'form-control', 'placeholder' => 'Name...']) }}
             @if ($errors->has('name'))
                 <span class="help-block">
